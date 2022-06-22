4558818e462db0a4e7ef323f3a448d2a7364a2f2
zainali186 <z.a.ghumman7788@gmail.com>
2022-06-23 02:05:56 +0500

    Updates

---
+++ precache-manifest.1e34bb07ea926867ab6949b478322138.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f6274c09e30a7490ac4",
+    "url": "/Servisco/static/css/main.df9b484e.chunk.css"
+  },
+  {
+    "revision": "6f6274c09e30a7490ac4",
+    "url": "/Servisco/static/js/main.6f6274c0.chunk.js"
+  },
+  {
+    "revision": "17b260c399b67066df61",
+    "url": "/Servisco/static/css/1.d8098b60.chunk.css"
+  },
+  {
+    "revision": "17b260c399b67066df61",
+    "url": "/Servisco/static/js/1.17b260c3.chunk.js"
+  },
+  {
+    "revision": "b27af418c0613fd47fca",
+    "url": "/Servisco/static/js/runtime~main.b27af418.js"
+  },
+  {
+    "revision": "d0eeb3eb6ed5a6a8c8b2b8b6e894df5d",
+    "url": "/Servisco/index.html"
+  }
+];
